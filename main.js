a4816019ff78c9e96625322aba805f8ec828b30c
Yousef Amar <yousefamar@gmail.com>
2017-03-17 11:05:59 +0000

    Use node-databox lib and test

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const databox = require('node-databox');
 
 const PORT = process.env.PORT || 8080;
 
 const HTTPS_SERVER_CERT = process.env.HTTPS_SERVER_CERT || '';
 const HTTPS_SERVER_PRIVATE_KEY = process.env.HTTPS_SERVER_PRIVATE_KEY || '';
+
+const store = process.env.DATABOX_DRIVER_MOBILE_DATABOX_STORE_BLOB_ENDPOINT;
 
 const app = express();
 
@@ -56,6 +59,23 @@ app.get('/is-connected', function(req, res){
 app.get('/set-mobile-ip', function(req, res){
 	mobileIp = req.query.ip;
 	res.end();
+});
+
+app.get('/do-stuff', function(req, res){
+	Promise.resolve().then(() => {
+		console.log('write', store);
+		return databox.keyValue.write(store, 'test', { foo: 'bar' });
+	}).then((response) => {
+		console.log(response);
+		console.log('read', store);
+		return databox.keyValue.read(store, 'test');
+	}).then((response) => {
+		console.log(response);
+		res.send();
+	}).catch((err) => {
+		console.error(err);
+		res.send();
+	});
 });
 
 https.createServer(credentials, app).listen(PORT);
